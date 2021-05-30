2a39ccd537f4e598f4dd49b96cef32b4e1d83cd8
NeaEnot <oleg.kozloss@yandex.ru>
2021-05-30 23:56:11 +0400

    Функция генерации текста

--- PozorNN/PozorNN.py
+++ PozorNN/PozorNN.py
@@ -12,12 +12,13 @@ dictionary = {}
 with open("Dictionary.json", 'r') as dump:
     dictionary = json.loads(dump.read())
 
+char_to_idx = dictionary
+idx_to_char = {v: k for k, v in char_to_idx.items()}
+
 def text_to_seq(text_sample):
-    char_to_idx = dictionary
-    idx_to_char = {v: k for k, v in char_to_idx.items()}
     sequence = np.array([char_to_idx[char] for char in text_sample])
     
-    return sequence, char_to_idx, idx_to_char
+    return sequence
 
 def get_batch():
     SEQ_LEN = 64
@@ -36,7 +37,7 @@ def get_batch():
                 for line in f:
                     text = text + line + '\n'
 
-        sequence, _, __ = text_to_seq(text)
+        sequence = text_to_seq(text)
 
         batch_start = np.random.randint(0, len(sequence) - SEQ_LEN)
         chunk = sequence[batch_start: batch_start + SEQ_LEN]
@@ -46,3 +47,24 @@ def get_batch():
         targets.append(target)
 
     return torch.stack(trains, dim=0), torch.stack(targets, dim=0)
+
+def evaluate(model, char_to_idx, idx_to_char, start_text=' ', prediction_len=200, temp=0.3):
+    hidden = model.init_hidden()
+    idx_input = [char_to_idx[char] for char in start_text]
+    train = torch.LongTensor(idx_input).view(-1, 1, 1).to(device)
+    predicted_text = start_text
+    
+    _, hidden = model(train, hidden)
+        
+    inp = train[-1].view(-1, 1, 1)
+    
+    for i in range(prediction_len):
+        output, hidden = model(inp.to(device), hidden)
+        output_logits = output.cpu().data.view(-1)
+        p_next = F.softmax(output_logits / temp, dim=-1).detach().cpu().data.numpy()        
+        top_index = np.random.choice(len(char_to_idx), p=p_next)
+        inp = torch.LongTensor([top_index]).view(-1, 1, 1).to(device)
+        predicted_char = idx_to_char[top_index]
+        predicted_text += predicted_char
+    
+    return predicted_text
